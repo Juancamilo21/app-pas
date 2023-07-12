61b18a7776ecd97928f5118d177cada691fc1622
Juancamilo21 <pruebajcab@gmail.com>
2023-07-12 10:16:17 -0500

    Adding environment variables explanation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,15 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyB5cP_lw8mysCvpdFRom-y3TnRXasECjRI",
-    authDomain: "app-pas-65333.firebaseapp.com",
-    databaseURL: "https://app-pas-65333-default-rtdb.firebaseio.com",
-    projectId: "app-pas-65333",
-    storageBucket: "app-pas-65333.appspot.com",
-    messagingSenderId: "224596087568",
-    appId: "1:224596087568:web:ccba91334d3c3754438b95"
-  }
+    apiKey: 'myApiKey',
+    authDomain: 'myAuthDomain',
+    databaseURL: 'mydatabaseURL',
+    projectId: 'myProjectId',
+    storageBucket: 'myStorageBucket',
+    messagingSenderId: 'myMessagingSenderId',
+    appId: 'myAppId',
+    measurementId: 'myMeasurementId',
+  },
 };
 
 /*
